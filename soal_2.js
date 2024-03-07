43fe6f37914fafb2ddb7e2e6b5178dedcaee1739
DickySyaifullah77 <161701641+DickySyaifullah77@users.noreply.github.com>
2024-03-07 17:00:34 +0700

    535230211 MUHAMMAD DICKY SYAIFULLAH

---
+++ soal_2.js
@@ -0,0 +1,26 @@
+const students = [
+    {
+        id:"535230211",
+        score: 98.0
+    }
+    {
+        id:"535230211",
+        score: 45.0
+    }
+    {
+        id:"535230211",
+        score: 97.75
+    }
+    {
+        id:"535230211",
+        score 99.25
+    }
+    {
+        id:"535230211",
+        score: 78.5
+    }
+];
+
+students.sort((a, b) => b.score - a.score);
+console.log(students);
+
